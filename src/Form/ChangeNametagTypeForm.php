8024e65b2fc9e57e20943fe60f528834e48bd818
Bogdan <bogdibogdan154@gmail.com>
2025-06-29 22:00:25 +0300

    Refactor interest update logic to replace old interest with new one and ensure uniqueness

---
+++ src/Form/ChangeNametagTypeForm.php
@@ -0,0 +1,34 @@
+<?php
+
+// src/Form/ChangeNametagType.php
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangeNametagTypeForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nametag', TextType::class, [
+                'label' => 'New Nametag',
+                'attr' => [
+                    'class' => 'form-control',
+                    'maxlength' => 30
+                ],
+                'required' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
+
