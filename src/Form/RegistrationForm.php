0a695740f6fa6ef0ad18f9d70dadc6480549affd
BDCosmin <cosmindanielbalan@gmail.com>
2025-08-09 18:47:16 +0300

    Implemented the choice of selecting a default avatar when registering

--- src/Form/RegistrationForm.php
+++ src/Form/RegistrationForm.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -95,6 +96,10 @@ class RegistrationForm extends AbstractType
                     ]),
                 ],
             ])
+            ->add('selectedAvatar', HiddenType::class, [
+                'mapped' => false,
+                'required' => false,
+            ]);
         ;
     }
 
